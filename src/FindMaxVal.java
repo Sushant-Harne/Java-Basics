2edc7eaa4149a5ede2cdf569458713c625afb8a7
Sushant Harne <venom.sh@outlook.com>
2026-09-24 16:20:43 +0530

    Basic Program to Find Largest number

---
+++ src/FindMaxVal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FindMaxVal {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int max = a;
+
+        if(b > max) {
+            if(b > c) {
+                max = b;
+            }else {
+                max = c;
+            }
+        } else if (c > max) {
+            max = c;
+        }
+
+        System.out.println(max);
+    }
+}
